439edc2c91e708758f0019f5f39dfdffe6ee4c78
ipatiy <nazarenkom130@gmail.com>
2025-08-29 23:15:39 +0300

    somthing

--- src/render/renderer.py
+++ src/render/renderer.py
@@ -82,18 +82,13 @@ def create_object_safely(cls, container, count):
                              rect_rect.right <= WIDTH2 and
                              rect_rect.bottom <= HEIGHT2)
 
-            all_items = container + objects
-            no_overlap = not any(rect_rect.colliderect(item.rect()) for item in all_items)
+            no_overlap = not any(rect_rect.colliderect(item.rect()) for item in container + objects)
 
             if inside_bounds and no_overlap:
                 objects.append(obj)
                 break
 
     return objects
-
-def smob(mob):
-    for mob_mob in mob:
-        return mob_mob
 
 obstacles = create_object_safely(Obstacle, container=[], count=4)
 finishes = create_object_safely(Finish, container=obstacles, count=1)
@@ -102,6 +97,9 @@ mob = create_object_safely(Mob, container=obstacles, count=1)
 pygame.display.set_caption('Pathfinder')  # Название окна
 sc = pygame.display.set_mode((WIDTH, HEIGHT))  # главное окно
 clock = pygame.time.Clock()
+
+mob = mob[0]
+finish = finishes[0]
 
 while True:
     for event in pygame.event.get():
@@ -112,21 +110,21 @@ while True:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT]:
-        smob(mob).x -= smob(mob).speed
-        if smob(mob).x < 0:
-            smob(mob).x = 0
+        mob.x -= mob.speed
+        if mob.x < 0:
+            mob.x = 0
     elif keys[pygame.K_RIGHT]:
-        smob(mob).x += smob(mob).speed
-        if smob(mob).x > 430:
-            smob(mob).x = 430
+        mob.x += mob.speed
+        if mob.x > 430:
+            mob.x = 430
     elif keys[pygame.K_DOWN]:
-        smob(mob).y += smob(mob).speed
-        if smob(mob).y > 330:
-            smob(mob).y = 330
+        mob.y += mob.speed
+        if mob.y > 330:
+            mob.y = 330
     elif keys[pygame.K_UP]:
-        smob(mob).y -= smob(mob).speed
-        if smob(mob).y < 0:
-            smob(mob).y = 0
+        mob.y -= mob.speed
+        if mob.y < 0:
+            mob.y = 0
 
     sc.fill(BLACK)
 
@@ -134,23 +132,24 @@ while True:
     surf = pygame.Surface((WIDTH2, HEIGHT2)) #рабочая область
     surf.fill(BLACK)
 
-    surf.blit(smob(mob).get_circle(), smob(mob).rect()) #размещаем на плоскости surf плоскость mob. smob(mob).rect() - по каким координатам
+    surf.blit(mob.get_circle(), mob.rect()) #размещаем на плоскости surf плоскость mob. smob(mob).rect() - по каким координатам
 
     pygame.draw.rect(surf, WHITE, (0, 0, WIDTH2, HEIGHT2), 2) #визуально выделяем рабочую область белым контуром по её границе
 
     for finish in finishes:
-        surf.blit(finish.get_finish(), finish .rect())
+        surf.blit(finish.get_finish(), finish.rect())
 
-        if finish .rect().contains(smob(mob).rect()):
+        if finish.rect().contains(mob.rect()):
             print('Вы полностью внутри!')
 
     for obstacle in obstacles:
         surf.blit(obstacle.get_obstacle(), obstacle.rect())
 
-        if obstacle.rect().contains(smob(mob).rect()):
+        if obstacle.rect().contains(mob.rect()):
             print('Вы полностью внутри!')
 
     sc.blit(surf, (MARGIN, MARGIN))
     pygame.display.update()
+
 
     clock.tick(FPS)
